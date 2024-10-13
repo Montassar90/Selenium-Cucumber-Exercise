aae7dd543a598bf0b9c5bd687fd35e76370c8803
Montassar90 <montassar.bahria0390@gmail.com>
2024-10-13 14:38:40 +0100

    Added Driver Setup File

---
+++ src/test/java/com/e2eTests/automation/utils/Setup.java
@@ -0,0 +1,23 @@
+package com.e2eTests.automation.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.cucumber.java.Before;
+
+public class Setup {
+	private static WebDriver driver;
+
+	@Before
+	public void setWebDriver() {
+
+		System.setProperty("webdriver.http.factory", "jdk-http-client");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+	}
+
+	// Getter
+	public static WebDriver getDriver() {
+		return driver;
+	}
+}
